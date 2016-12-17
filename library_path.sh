04531dea289c615b596feaf02e400a96ce06e4e8
Sami Lefebvre <sami.lefebvre@gmail.com>
2016-12-17 15:33:42 +0100

    On va tenter d'utiliser git correctement cette fois-ci

--- library_path.sh
+++ library_path.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#Sert à indiquer que les bibliothèques sont dans le répertoire courant.
-echo export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH >> ~/.profile
+#la condition est pas top, elle empêche pas d'avoir quelque chose du genre LD_LIBRARY_PATH=coucou:.:salut:.:chose:.:
+#Mais si on lance le script plusieurs fois par erreur, on n'aura pas une variable $LD_LIBRARY_PATH absolument immondedu genre .:.:.:.:.:
+if test -z "$(cat ~/.bashrc | grep 'export LD_LIBRARY_PATH=.:')"
+then
+	#Sert à indiquer que les bibliothèques sont dans le répertoire courant.
+	echo export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH >> ~/.profile
+else
+	echo "Le répertoire courant est déjà présent dans \$LD_LIBRARY_PATH"
+fi
 
 #On met les droits d'éxecution aussi, au cas où.
 chmod u+x Combat
