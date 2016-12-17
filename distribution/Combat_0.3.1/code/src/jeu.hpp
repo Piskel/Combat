f734837801a399031c8bf3c58866bb5558d7d52d
Sami Lefebvre <sami.lefebvre@gmail.com>
2016-12-17 15:39:49 +0100

    Suppression de certains fichiers inutiles et création du .gitignore

--- distribution/Combat_0.3.1/code/src/jeu.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <SFML/Graphics.hpp>
-
-#include "Joueur.hpp"
-
-int jeu(sf::RenderWindow *fenetre, int type, Joueur *j1, Joueur *j2);
-int pause(sf::RenderWindow *fenetre, sf::View vue);
--- distribution/Combat_0.3.1/code/src/main.hpp
+++
@@ -1,3 +0,0 @@
-#include <SFML/Graphics.hpp>
-
-int credits(sf::RenderWindow *fenetre);
